dbe85f2a4e527dda04d38d5062e3942c7b99b535
fr13t <fr13thinkpad@outlook.com>
2026-01-29 12:27:14 +0700

    FLuxpert v1

--- create_integration_test_folder.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-echo Creating integration_test folder...
-cd /d "%~dp0"
-mkdir integration_test 2>nul
-echo ✓ Folder created
--- create_utils_folder.bat
+++
@@ -1,3 +0,0 @@
-@echo off
-if not exist "D:\Dicoding\Fluxpert01\lib\core\utils" mkdir "D:\Dicoding\Fluxpert01\lib\core\utils"
-echo Utils directory created
